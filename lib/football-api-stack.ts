3af0db0afef395d77c5e9a91395cc2a5035e6cbd
BruceWan1021 <brucewan1021@foxmail.com>
2025-03-22 21:12:54 +0000

    create a POST request in football-api-stacks.ts

--- lib/football-api-stack.ts
+++ lib/football-api-stack.ts
@@ -1,16 +1,64 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apig from "aws-cdk-lib/aws-apigateway";
 
-export class FootballApiStack extends cdk.Stack {
+export class RestAPIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Tables 
+    const matchesTable = new dynamodb.Table(this, "FootballMatchesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "FootballMatches",
+    });
+    
+    // Functions
+    // Add Match 
+    const addMatchFn = new lambdanode.NodejsFunction(
+      this,
+      "AddMatchFunction",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/addMatch.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: matchesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+      );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FootballApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // Permissions 
+      matchesTable.grantWriteData(addMatchFn)
+     
+
+      const api = new apig.RestApi(this, "FootballAPI", {
+        description: "Football Match API",
+        deployOptions: {
+          stageName: "dev",
+        },
+        defaultCorsPreflightOptions: {
+          allowHeaders: ["Content-Type", "X-Amz-Date"],
+          allowMethods: ["OPTIONS", "GET", "POST"],
+          allowCredentials: true,
+          allowOrigins: ["*"],
+        },
+      });
+    
+      const matchesEndpoint = api.root.addResource("matches");
+      matchesEndpoint.addMethod(
+        "POST",
+        new apig.LambdaIntegration(addMatchFn, { proxy: true })
+      );
+    }
   }
-}
+    
